fd12ed692162cf16d1787fbe1d1368dbf4cce8f5
zhonghui <zhonghui324@gmail.com>
2026-04-27 23:30:36 +0800

    fix: 更新 vite 脚本，生产环境使用 ./ 路径

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   plugins: [react(), basicSsl()],
   resolve: {
     alias: {
